6b9411ed720623884a4eab5c13eaf60d3323c509
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-11-10 15:37:07 -0300

    clean up

--- UnoNetConf/ViewModels/PlaygroundViewModel.cs
+++ UnoNetConf/ViewModels/PlaygroundViewModel.cs
@@ -1,18 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace UnoNetConf.ViewModels;
 partial class PlaygroundViewModel : ObservableObject
 {
-	[ObservableProperty]
-	int count;
 
-	[RelayCommand]
-	void IncreaseCount()
-	{
-		Count++;
-	}
 }
